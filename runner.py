cf980f2bf022c46f6b5dc12bd28e1fd8e720f81b
Buranosky <buranosky.matthew@epa.gov>
2018-06-20 10:42:02 -0400

    add time limit

--- runner.py
+++ runner.py
@@ -1,6 +1,21 @@
 """Convenience wrapper for running fdtool directly from source tree."""
-
+import multiprocessing, time, sys
 from fdtool.fdtool import main
 
 if __name__ == '__main__':
-    main()
+    
+    # Start main as a process
+    p = multiprocessing.Process(target=main, name="Main")
+    p.start()
+    # Wait 4 hours for main
+    p.join(1440)
+    
+    # If thread is active
+    if p.is_alive():
+        # Print exceeded time limit
+        print "\n","Exceeded preset time limit of 4 hours."; sys.stdout.flush()
+        # Terminate main
+        p.terminate()
+        # Cleanup
+        p.join()
+
