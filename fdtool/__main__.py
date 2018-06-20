077bc6bd6a0d202a86e69cb35babd7b22466c6c5
Buranosky <buranosky.matthew@epa.gov>
2018-06-20 10:56:14 -0400

    add preset time limit

--- fdtool/__main__.py
+++ fdtool/__main__.py
@@ -1,6 +1,18 @@
-
-
 """fdtool.__main__: executed when fdtool directory is called as script."""
-
+import multiprocessing, time, sys
 from fdtool import main
-main()
+
+# Start main as a process
+p = multiprocessing.Process(target=main, name="Main")
+p.start()
+# Wait 4 hours for main
+p.join(1440)
+
+# If thread is active
+if p.is_alive():
+    # Print exceeded time limit
+    print "\n", "Exceeded preset time limit of 4 hours."; sys.stdout.flush()
+    # Terminate main
+    p.terminate()
+    # Cleanup
+    p.join()
