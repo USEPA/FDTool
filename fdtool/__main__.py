ec8d28a63d647873f3ede6bc970d863dbd32fa7d
Giorgi Luca (Student Com18) <luca.giorgi@stud-inf.unibz.it>
2019-06-28 01:17:30 +0200

    2 to 3

--- fdtool/__main__.py
+++ fdtool/__main__.py
@@ -1,20 +1,20 @@
-"""fdtool.__main__: executed when fdtool directory is called as script."""
-import multiprocessing, time, sys
-from fdtool import main
-from config import MAX_TIME
-
-
-# Start main as a process
-p = multiprocessing.Process(target=main, name="Main")
-p.start()
-# Wait for main
-p.join(MAX_TIME)
-
-# If thread is active
-if p.is_alive():
-    # Print exceeded time limit
-    print "\n", "Exceeded preset time limit."; sys.stdout.flush()
-    # Terminate main
-    p.terminate()
-    # Cleanup
-    p.join()
+"""fdtool.__main__: executed when fdtool directory is called as script."""
+import multiprocessing, time, sys
+from .fdtool import main
+from .config import MAX_TIME
+
+
+# Start main as a process
+p = multiprocessing.Process(target=main, name="Main")
+p.start()
+# Wait for main
+p.join(MAX_TIME)
+
+# If thread is active
+if p.is_alive():
+    # Print exceeded time limit
+    print("\n", "Exceeded preset time limit."); sys.stdout.flush()
+    # Terminate main
+    p.terminate()
+    # Cleanup
+    p.join()
